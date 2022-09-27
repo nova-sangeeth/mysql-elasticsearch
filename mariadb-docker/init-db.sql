df5bc2dba7c0cb1f832b109068acc2a2ed5649a3
nova-sangeeth <novasangeeth@outlook.com>
2022-09-28 00:24:32 +0530

    uncomment the insert and create table statements.

--- mariadb-docker/init-db.sql
+++ mariadb-docker/init-db.sql
@@ -5,52 +5,57 @@ CREATE USER 'instaqp' @'localhost' IDENTIFIED BY 'passw0rd';
 CREATE USER 'instaqp' @'%' IDENTIFIED BY 'passw0rd';
 -- giving permission to instaqp user account to the databases
 GRANT ALL PRIVILEGES ON instaqp.* TO 'instaqp' @'%';
-
+-- 
 FLUSH PRIVILEGES;
-
--- CREATE TABLE es_table (
---     id BIGINT(20) UNSIGNED NOT NULL,
---     PRIMARY KEY (id),
---     UNIQUE KEY unique_id (id),
---     client_name VARCHAR(32) NOT NULL,
---     modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
--- );
--- CREATE TABLE es_table_2 (
---     id BIGINT(20) UNSIGNED NOT NULL,
---     PRIMARY KEY (id),
---     UNIQUE KEY unique_id (id),
---     client_name VARCHAR(32) NOT NULL,
---     modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
--- );
--- CREATE TABLE es_table_3 (
---     id BIGINT(20) UNSIGNED NOT NULL,
---     PRIMARY KEY (id),
---     UNIQUE KEY unique_id (id),
---     client_name VARCHAR(32) NOT NULL,
---     modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
--- );
-
--- INSERT INTO es_table (id, client_name)
--- VALUES (1, "test one"),
---     (2, "test two"),
---     (3, "test three"),
---     (4, "test four"),
---     (5, "new test five"),
---     (6, "test six"),
---     (7, "test seven");
--- INSERT INTO es_table_2 (id, client_name)
--- VALUES (1, "test one"),
---     (2, "test two"),
---     (3, "test three"),
---     (4, "test four"),
---     (5, "new test five"),
---     (6, "test six"),
---     (7, "test seven");
--- INSERT INTO es_table_3 (id, client_name)
--- VALUES (1, "test one"),
---     (2, "test two"),
---     (3, "test three"),
---     (4, "test four"),
---     (5, "new test five"),
---     (6, "test six"),
---     (7, "test seven");
+-- 
+CREATE TABLE es_table (
+    id BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY unique_id (id),
+    client_name VARCHAR(32) NOT NULL,
+    modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- 
+CREATE TABLE es_table_2 (
+    id BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY unique_id (id),
+    client_name VARCHAR(32) NOT NULL,
+    modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- 
+CREATE TABLE es_table_3 (
+    id BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY unique_id (id),
+    client_name VARCHAR(32) NOT NULL,
+    modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- 
+INSERT INTO es_table (id, client_name)
+VALUES (1, "test one"),
+    (2, "test two"),
+    (3, "test three"),
+    (4, "test four"),
+    (5, "new test five"),
+    (6, "test six"),
+    (7, "test seven");
+-- 
+INSERT INTO es_table_2 (id, client_name)
+VALUES (1, "test one"),
+    (2, "test two"),
+    (3, "test three"),
+    (4, "test four"),
+    (5, "new test five"),
+    (6, "test six"),
+    (7, "test seven");
+-- 
+INSERT INTO es_table_3 (id, client_name)
+VALUES (1, "test one"),
+    (2, "test two"),
+    (3, "test three"),
+    (4, "test four"),
+    (5, "new test five"),
+    (6, "test six"),
+    (7, "test seven");
+--
