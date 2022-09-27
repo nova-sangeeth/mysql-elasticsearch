3c4f347df1d46cd904b454aa172464b84bd29a3b
nova-sangeeth <novasangeeth@outlook.com>
2022-09-28 00:22:29 +0530

    add new index.

--- app/create.py
+++ app/create.py
@@ -8,7 +8,9 @@ from main import es_client
 # index_names
 index_name_1 = "my_index_1"
 index_name_2 = "my_index_2"
+index_name_3 = "my_index_2"
 
 # create new indexes
 es_client.indices.create(index=index_name_1, body=index_schema_1)
 es_client.indices.create(index=index_name_2, body=index_schema_2)
+# es_client.indices.create(index=index_name_3, body=index_schema_2)
