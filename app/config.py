2400310e47ed30cf95ace75843c2dc384c362f6f
nova-sangeeth <novasangeeth@outlook.com>
2022-09-30 02:29:42 +0530

    Add the db_config to the config.py

--- app/config.py
+++ app/config.py
@@ -1,2 +1,13 @@
 ELASTICSEARCH_HOST_URL = "http://localhost:9200"
 DEBUG = True
+
+
+DATABASE_CONFIG = {
+    "host": "localhost",
+    "port": "3306",
+    "user": "instaqp",
+    "password": "passw0rd",
+    "database": "instaqp",
+    "dialect": "mysql",
+    "connector": "mysqlconnector",
+}
