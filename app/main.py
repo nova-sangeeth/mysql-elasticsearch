0b05729c4f2971c7ca76f33d35165d8bae53387a
nova-sangeeth <novasangeeth@outlook.com>
2022-09-30 00:28:31 +0530

    move flask to a separate file.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,4 @@
 from elasticsearch import Elasticsearch
-from config import ELASTICSEARCH_HOST_URL, DEBUG
-from flask import Flask
+from config import ELASTICSEARCH_HOST_URL
 
-app = Flask(__name__)
 es_client = Elasticsearch(hosts=ELASTICSEARCH_HOST_URL)
-
-
-@app.route("/")
-def home():
-    message = {"message": "App works."}
-    return message
-
-
-if __name__ == "__main__":
-    app.run(debug=DEBUG)
