6f3d84553a5a245c9c1e659bc882631f452dc245
nova-sangeeth <novasangeeth@outlook.com>
2022-09-29 14:52:13 +0530

    Add a flask application along with the es_client.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from elasticsearch import Elasticsearch
-from config import HOST_URL
+from config import ELASTICSEARCH_HOST_URL, DEBUG
+from flask import Flask
+
+app = Flask(__name__)
+es_client = Elasticsearch(hosts=ELASTICSEARCH_HOST_URL)
 
 
-es_client = Elasticsearch(
-    hosts=HOST_URL,
-)
+@app.route("/")
+def home():
+    message = {"message": "App works."}
+    return message
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG)
